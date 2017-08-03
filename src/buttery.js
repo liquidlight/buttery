cf1cb31f99070b2fa9e72e2419a095c65a542fb8
Mike Street <mike@liquidlight.co.uk>
2017-08-03 10:15:42 +0100

    Bug: Add min-height of the element, so it is never shorter

--- src/buttery.js
+++ src/buttery.js
@@ -91,6 +91,7 @@
 				'top': '0',
 				'left': '0',
 				'width': '100%',
+				'minHeight': parallaxWrapper.height(),
 				'height': itemOptions.height,
 				'transformOrigin': 'top center',
 				'transition': 'transform 5ms linear',
